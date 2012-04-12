b00d8b47f851281141b0b996aef8f6a8e4d72c69
Stian Soiland-Reyes <stian@soiland-reyes.com>
2012-04-12 15:58:23 +0100

    Icon for astro service

---
+++ astrotaverna-voservices/src/main/java/uk/org/taverna/astro/vo/VOServiceIcon.java
@@ -0,0 +1,31 @@
+package uk.org.taverna.astro.vo;
+
+import javax.swing.Icon;
+
+import net.sf.taverna.t2.activities.rest.RESTActivity;
+import net.sf.taverna.t2.workbench.activityicons.ActivityIconSPI;
+import net.sf.taverna.t2.workflowmodel.processor.activity.Activity;
+
+public class VOServiceIcon implements ActivityIconSPI {
+
+	@Override
+	public int canProvideIconScore(Activity<?> activity) {
+		if (activity instanceof RESTActivity) {
+			RESTActivity restActivity = (RESTActivity) activity;
+			if (isAstro(restActivity));
+			return ActivityIconSPI.DEFAULT_ICON + 100;
+		}
+		return ActivityIconSPI.NO_ICON;
+	}
+
+	public boolean isAstro(RESTActivity restActivity) {
+		// TEST: Check this.. somehow!
+		return true;
+	}
+
+	@Override
+	public Icon getIcon(Activity<?> arg0) {
+		return VOServicesPerspective.voIcon;
+	}
+
+}
