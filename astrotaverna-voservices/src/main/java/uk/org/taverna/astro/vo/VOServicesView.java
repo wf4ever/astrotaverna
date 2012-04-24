1ca037c4c5e3573d9b005a962d279b6a2bf202b1
Stian Soiland-Reyes <stian@soiland-reyes.com>
2012-04-24 11:25:50 +0100

    WFE-350 More complete service information

--- astrotaverna-voservices/src/main/java/uk/org/taverna/astro/vo/VOServicesView.java
+++ astrotaverna-voservices/src/main/java/uk/org/taverna/astro/vo/VOServicesView.java
@@ -7,6 +7,7 @@ import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.net.URI;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CancellationException;
@@ -27,10 +28,28 @@ import javax.swing.event.ListSelectionListener;
 import javax.swing.table.DefaultTableModel;
 
 import net.ivoa.xml.conesearch.v1.ConeSearch;
+import net.ivoa.xml.conesearch.v1.Query;
+import net.ivoa.xml.sia.v1.ImageSize;
 import net.ivoa.xml.sia.v1.SimpleImageAccess;
+import net.ivoa.xml.sia.v1.SkySize;
+import net.ivoa.xml.slap.v0.SimpleLineAccess;
+import net.ivoa.xml.ssa.v0.DataSource;
 import net.ivoa.xml.ssa.v0.SimpleSpectralAccess;
+import net.ivoa.xml.vodataservice.v1.HTTPQueryType;
+import net.ivoa.xml.vodataservice.v1.InputParam;
+import net.ivoa.xml.vodataservice.v1.ParamHTTP;
+import net.ivoa.xml.voresource.v1.AccessURL;
 import net.ivoa.xml.voresource.v1.Capability;
+import net.ivoa.xml.voresource.v1.Contact;
+import net.ivoa.xml.voresource.v1.Content;
+import net.ivoa.xml.voresource.v1.Creator;
+import net.ivoa.xml.voresource.v1.Curation;
+import net.ivoa.xml.voresource.v1.Date;
+import net.ivoa.xml.voresource.v1.Interface;
+import net.ivoa.xml.voresource.v1.ResourceName;
 import net.ivoa.xml.voresource.v1.Service;
+import net.ivoa.xml.voresource.v1.Source;
+import net.ivoa.xml.voresource.v1.Type;
 import net.sf.taverna.t2.workbench.ui.zaria.UIComponentSPI;
 
 import org.apache.log4j.Logger;
@@ -415,16 +434,335 @@ public class VOServicesView extends JPanel implements UIComponentSPI {
 			return;
 		}
 
-		String message = String.format("<html><body><h3>%s: %s</h3>"
-				+ "<p>%s</p>" + "<dl><dt>Publisher</dt> <dd>%s</dd>"
-				+ "  <dt>Documentation</dt> <dd><a href='%s'>%s</a></dd>"
-				+ "</dl>", service.getShortName(), service.getTitle(), service
-				.getContent().getDescription(), service.getCuration()
-				.getPublisher().getValue(), service.getContent()
-				.getReferenceURL(), service.getContent().getReferenceURL());
-		htmlPane.setText(message);
+		StringBuffer message = new StringBuffer();
+		appendFormat(message, "<html><body><h2>%s: %s</h2>",				
+				service.getShortName(), service.getTitle());
+
+		Content content = service.getContent();
+		if (content != null) {
+			
+			message.append("<div><em>");
+			for (Type t : content.getType()) {
+				message.append(t.name());
+				message.append(" ");
+			}
+			message.append("</em></div>");
+			appendFormat(message, "<div>%s</div>", content.getDescription());
+			
+			message.append("<dl>");
+			Source source = content.getSource();
+			if (source != null) {
+				message.append("<dt>Source</dt>");
+				appendFormat(message, "<dd>%s <code>%s</code></dd>",
+						source.getValue(), source.getFormat());
+			}
+			if (!content.getSubject().isEmpty()) {
+				message.append("<dt>Subjects</dt>");
+				for (String s : content.getSubject()) {
+					appendFormat(message, "<dd>%s</dd>", s);					
+				}
+			}
+		}
+		message.append("</dl>");
+
+
+		
+		for (Capability c : service.getCapability()) {
+			boolean seenCap = false;
+			for (Interface i : c.getInterface()) {
+				if (!(i instanceof ParamHTTP)) {
+					continue;
+					// TODO: Handle WebService interface?
+				}
+				if (!seenCap) {
+					seenCap = true;
+					message.append("<h3>Service</h3>");
+					appendFormat(message, "<div><code>%s</code></div>", c.getStandardID());
+					appendFormat(message, "<div>%s</div>", c.getDescription());
+					if (c instanceof ConeSearch) {
+						ConeSearch cone = (ConeSearch) c;
+						message.append("<dl>");
+						if (cone.isVerbosity()) {
+							message.append("<dt>Verbose</dt>");
+						}
+						appendFormat(message,
+								"<dt>Maximum records</dt><dd>%s</dd>",
+								cone.getMaxRecords());
+
+						appendFormat(message,
+								"<dt>Maximum search radius</dt><dd>%s</dd>",
+								cone.getMaxSR());
+
+						if (cone.getTestQuery() != null) {
+							message.append("<dt>Test query</dt><dd>");
+							message.append("<dl>");
+							Query tq = cone.getTestQuery();
+							appendFormat(message,
+									"<dt>Catalogue</dt><dd>%s</dd>",
+									tq.getCatalog());
+							appendFormat(message, "<dt>SR</dt><dd>%s</dd>",
+									tq.getSr());
+							appendFormat(message, "<dt>DEC</dt><dd>%s</dd>",
+									tq.getDec());
+							appendFormat(message, "<dt>RA</dt><dd>%s</dd>",
+									tq.getRa());
+							appendFormat(message, "<dt>Extras</dt><dd>%s</dd>",
+									tq.getExtras());
+							appendFormat(message, "<dt>Verb</dt><dd>%s</dd>",
+									tq.getVerb());
+							message.append("</dl>");
+							message.append("</dd>");
+						}
+
+						message.append("</dl>");
+
+					}
+
+					if (c instanceof SimpleLineAccess) {
+						SimpleLineAccess sla = (SimpleLineAccess) c;
+						message.append("<dl>");
+
+						appendFormat(message,
+								"<dt>Maximum records</dt><dd>%s</dd>",
+								sla.getMaxRecords());
+						appendFormat(message, "<dt>Source</dt><dd>%s</dd>",
+								sla.getDataSource());
+
+						if (sla.getTestQuery() != null) {
+							message.append("<dt>Test query</dt><dd>");
+							message.append("<dl>");
+							net.ivoa.xml.slap.v0.Query tq = sla.getTestQuery();
+							appendFormat(message,
+									"<dt>Wavelength</dt><dd>%s - %s</dd>",
+									tq.getWavelength().getMinWavelength(), tq.getWavelength().getMaxWavelength());
+							appendFormat(message,
+									"<dt>Query data cmd</dt><dd>%s</dd>",
+									tq.getQueryDataCmd());
+							message.append("</dl>");
+							message.append("</dd>");
+						}
+						message.append("</dl>");
+					}
+
+					if (c instanceof SimpleImageAccess) {
+						SimpleImageAccess sia = (SimpleImageAccess) c;
+						message.append("<dl>");
+
+						appendFormat(message,
+								"<dt>Maximum records</dt><dd>%s</dd>",
+								sia.getMaxRecords());
+
+						appendFormat(message,
+								"<dt>Image service type</dt><dd>%s</dd>",
+								sia.getImageServiceType());
+						if (sia.getMaxQueryRegionSize() != null) {
+							SkySize size = sia.getMaxQueryRegionSize();
+							message.append("<dt>Maximum query region size</dt>");
+							appendFormat(message, "<dd>Lat: %s, Long: %s</dd>",
+									size.getLat(), size.getLong());
+						}
+						if (sia.getMaxImageExtent() != null) {
+							SkySize size = sia.getMaxImageExtent();
+							message.append("<dt>Maximum image extent</dt>");
+							appendFormat(message, "<dd>Lat: %s, Long: %s</dd>",
+									size.getLat(), size.getLong());
+						}
+						if (sia.getMaxImageSize() != null) {
+							ImageSize size = sia.getMaxImageSize();
+							message.append("<dt>Maximum image size</dt>");
+							appendFormat(message, "<dd>Lat: %s, Long: %s</dd>",
+									size.getLat(), size.getLong());
+						}
+
+						appendFormat(message,
+								"<dt>Maximum file size</dt><dd>%s</dd>",
+								sia.getMaxFileSize());
+
+						if (sia.getTestQuery() != null) {
+							message.append("<dt>Test query</dt><dd>");
+							message.append("<dl>");
+							net.ivoa.xml.sia.v1.Query tq = sia.getTestQuery();
+							appendFormat(message, "<dt>POS</dt><dd>Lat: %s, Long: %s</dd>",
+									tq.getPos().getLat(), tq.getPos().getLong());
+							appendFormat(message, "<dt>SIZE</dt><dd>Lat: %s, Long: %s</dd>",
+									tq.getSize().getLat(), tq.getSize().getLong());
+							appendFormat(message, "<dt>Extras</dt><dd>%s</dd>",
+									tq.getExtras());
+							appendFormat(message, "<dt>Verb</dt><dd>%s</dd>",
+									tq.getVerb());
+							message.append("</dl>");
+							message.append("</dd>");
+						}
+						message.append("</dl>");
+					}
+					if (c instanceof SimpleSpectralAccess) {
+						SimpleSpectralAccess spa = (SimpleSpectralAccess) c;
+						message.append("<dl>");
+
+						appendFormat(message,
+								"<dt>Maximum records</dt><dd>%s</dd>",
+								spa.getMaxRecords());
+
+						appendFormat(message,
+								"<dt>Maximum search radius</dt><dd>%s</dd>",
+								spa.getMaxSearchRadius());
+						appendFormat(message,
+								"<dt>Maximum aperture</dt><dd>%s</dd>",
+								spa.getMaxAperture());
+
+						appendFormat(message,
+								"<dt>Maximum file size</dt><dd>%s</dd>",
+								spa.getMaxFileSize());
+						if (!spa.getDataSource().isEmpty()) {
+							message.append("<dt>Sources</dt>");
+							for (DataSource source : spa.getDataSource()) {
+								appendFormat(message, "<dd>%s</dd>", source);
+							}
+						}
+
+						if (!spa.getSupportedFrame().isEmpty()) {
+							message.append("<dt>Supported frames</dt>");
+							for (String frame : spa.getSupportedFrame()) {
+								appendFormat(message, "<dd>%s</dd>", frame);
+							}
+						}
+
+						if (spa.getTestQuery() != null) {
+							message.append("<dt>Test query</dt><dd>");
+							message.append("<dl>");
+							net.ivoa.xml.ssa.v0.Query tq = spa.getTestQuery();
+							appendFormat(message, "<dt>POS</dt><dd>Lat: %s, Long: %s</dd>",
+									tq.getPos().getLat(), tq.getPos().getLong());
+							appendFormat(message, "<dt>SIZE</dt><dd>%s</dd>",
+									tq.getSize());
+							appendFormat(message,
+									"<dt>Query data cmd</dt><dd>%s</dd>",
+									tq.getQueryDataCmd());
+							message.append("</dl>");
+							message.append("</dd>");
+						}
+						message.append("</dl>");
+					}
+
+				}
+
+				ParamHTTP paramHTTP = (ParamHTTP) i;
+
+				HTTPQueryType queryType = paramHTTP.getQueryType();
+				if (queryType == null) {
+					queryType = HTTPQueryType.GET;
+				}
+				for (AccessURL accessURL : i.getAccessURL()) {
+					message.append(String.format("<p><code>%s %s</code></p>", queryType,
+							accessURL.getValue().trim()));
+				}
+				appendFormat(message, "<div><em>Version: %s</em></div>", paramHTTP.getVersion());
+				appendFormat(message,
+						"<div>Result type: <code>%s</code></div>",
+						paramHTTP.getResultType());
+				if (paramHTTP.getParam() != null
+						&& !paramHTTP.getParam().isEmpty()) {
+					message.append("<h4>Input parameters</h4><dl>");
+					for (InputParam param : paramHTTP.getParam()) {
+						message.append("<dt>");
+						message.append(param.getName());
+						message.append(" (");
+						message.append(param.getUse());
+						message.append(" )");
+						message.append("</dt><dd>");
+						if (param.getUnit() != null) {
+							message.append(param.getUnit());
+						}
+						if (param.getDataType() != null) {
+							message.append(" <code>");
+							message.append(param.getDataType().getValue());
+							message.append(" [");
+							message.append(param.getDataType().getArraysize());
+							message.append("]");
+							message.append("</code>");
+						}
+						message.append("</dd>");
+						appendFormat(message, "<dd><em>%s</em></dd>",
+								param.getDescription());
+					}
+					message.append("</dl>");
+				}
+			}
+
+		}
+		message.append("<h3>More information</h3><dl>");
+		appendFormat(message, "<dt>Identifier</dt><dd>%s</dd>",
+				service.getIdentifier());
+		appendFormat(message, "<dt>Status</dt><dd>%s</dd>", service.getStatus());
+		if (service.getUpdated() != null) {
+			appendFormat(
+					message,
+					"<dt>Updated</dt><dd>%s</dd>",
+					new SimpleDateFormat().format(service.getUpdated()
+							.toGregorianCalendar().getTime()));
+		}
+		Curation curation = service.getCuration();
+		if (curation != null) {
+			appendFormat(message, "<dt>Publisher</dt><dd>%s</dd>", curation
+					.getPublisher().getValue());
+			if (!curation.getContributor().isEmpty()) {
+				message.append("<dt>Contributor</dt>");
+				for (ResourceName contrib : curation.getContributor()) {
+					appendFormat(message, "<dd>%s</dd>", contrib.getValue());
+				}
+			}
+			if (!curation.getCreator().isEmpty()) {
+				message.append("<dt>Creator</dt>");
+				for (Creator creator : curation.getCreator()) {
+					appendFormat(message, "<dd>%s</dd>", creator.getName()
+							.getValue());
+				}
+			}
+			if (!curation.getCreator().isEmpty()) {
+				message.append("<dt>Contact</dt>");
+				for (Contact contact : curation.getContact()) {
+					appendFormat(message, "<dd>%s &lt;<a href='mailto:%s'>%s</a>&gt; "
+							+ "<br><a href='tel:%s'>%s</a><br>%s</dd>", contact
+							.getName().getValue(), contact.getEmail(),
+							contact.getEmail(), contact.getTelephone(),
+							contact.getTelephone(), contact.getAddress());
+				}
+			}
+			appendFormat(message, "<dt>Version</dt><dd>%s</dd>",
+					curation.getVersion());
+			if (!curation.getDate().isEmpty()) {
+				message.append("<dt>Date</dt>");
+				for (Date date : curation.getDate()) {
+					appendFormat(message, "<dd>%s %s</dd>", date.getValue(),
+							date.getRole());
+				}
+			}
+		}
+		appendFormat(message, "<dt>Reference</dt><dd><a href='%s'>%s</a></dd>",
+				service.getContent().getReferenceURL(), service.getContent()
+						.getReferenceURL());
+
+		message.append("</dl></body></html>");
+
+		htmlPane.setText(message.toString());
 		addToWorkflow.setEnabled(true);
 
+	}
+
+	private void appendFormat(StringBuffer message, String formatStr,
+			Object... args) {
+		boolean foundAny = false;
+		for (int i = 0; i < args.length; i++) {
+			if (args[i] == null) {
+				args[i] = "";
+			} else {
+				foundAny = true;
+			}
+		}
+		if (!foundAny) {
+			return;
+		}
+		message.append(String.format(formatStr, args));
 	}
 
 	public void updateSelection() {
