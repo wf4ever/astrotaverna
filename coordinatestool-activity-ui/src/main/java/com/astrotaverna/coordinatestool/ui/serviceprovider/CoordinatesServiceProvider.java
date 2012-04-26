21cc3fa9d8acd053437f93abfe9a10c993bdc4d9
Stian Soiland-Reyes <stian@soiland-reyes.com>
2012-04-26 15:34:35 +0100

    service provider does not need to be configurable

--- coordinatestool-activity-ui/src/main/java/com/astrotaverna/coordinatestool/ui/serviceprovider/CoordinatesServiceProvider.java
+++ coordinatestool-activity-ui/src/main/java/com/astrotaverna/coordinatestool/ui/serviceprovider/CoordinatesServiceProvider.java
@@ -7,23 +7,20 @@ import java.util.List;
 
 import javax.swing.Icon;
 
-import net.sf.taverna.t2.servicedescriptions.AbstractConfigurableServiceProvider;
-import net.sf.taverna.t2.servicedescriptions.ConfigurableServiceProvider;
 import net.sf.taverna.t2.servicedescriptions.ServiceDescription;
 import net.sf.taverna.t2.servicedescriptions.ServiceDescriptionProvider;
+import net.sf.taverna.t2.servicedescriptions.ServiceDescriptionProvider.FindServiceDescriptionsCallBack;
 
 //public class coordinatesServiceProvider implements ServiceDescriptionProvider {
-public class CoordinatesServiceProvider extends 
-AbstractConfigurableServiceProvider<CoordinatesServiceProviderConfig> implements 
-ConfigurableServiceProvider<CoordinatesServiceProviderConfig> {
-	
-	public CoordinatesServiceProvider() {
-		super(new CoordinatesServiceProviderConfig());
+public class CoordinatesServiceProvider implements
+		ServiceDescriptionProvider {
+
+	public CoordinatesServiceProvider() {	
 	}
 
 	private static final URI providerId = URI
-		.create("http://example.com/2011/service-provider/coordinatestool");
-	
+			.create("http://example.com/2011/service-provider/coordinatestool");
+
 	/**
 	 * Do the actual search for services. Return using the callBack parameter.
 	 */
@@ -37,27 +34,26 @@ ConfigurableServiceProvider<CoordinatesServiceProviderConfig> {
 
 		// FIXME: Implement the actual service search/lookup instead
 		// of dummy for-loop
-		//for (int i = 1; i <= 5; i++) {
-			CoordinatesServiceDesc service = new CoordinatesServiceDesc();
-			// Populate the service description bean
-			service.setExampleString("Equatorials2Galactic");
-			//service.setExampleUri(URI.create("http://localhost:8192/service"));
+		// for (int i = 1; i <= 5; i++) {
+		CoordinatesServiceDesc service = new CoordinatesServiceDesc();
+		// Populate the service description bean
+		service.setExampleString("Equatorials2Galactic");
+		// service.setExampleUri(URI.create("http://localhost:8192/service"));
 
-			// Optional: set description
-			//service.setDescription("Service example number " + i);
-			results.add(service);
-			
-			CoordinateDegreeServiceDesc service2 = new CoordinateDegreeServiceDesc();
-			service2.setExampleString("hms2degrees");
-			results.add(service2);
-		//}
-//		for (int i = 1; i <= getConfiguration().getNumberOfServices(); i++) {
-//			coordinatesServiceDesc service = new coordinatesServiceDesc();
-//			service.setExampleString("Example " + i);
-//			service.setExampleUri(getConfiguration().getUri());
-//			results.add(service);
-//		}
-		
+		// Optional: set description
+		// service.setDescription("Service example number " + i);
+		results.add(service);
+
+		CoordinateDegreeServiceDesc service2 = new CoordinateDegreeServiceDesc();
+		service2.setExampleString("hms2degrees");
+		results.add(service2);
+		// }
+		// for (int i = 1; i <= getConfiguration().getNumberOfServices(); i++) {
+		// coordinatesServiceDesc service = new coordinatesServiceDesc();
+		// service.setExampleString("Example " + i);
+		// service.setExampleUri(getConfiguration().getUri());
+		// results.add(service);
+		// }
 
 		// partialResults() can also be called several times from inside
 		// for-loop if the full search takes a long time
@@ -79,23 +75,16 @@ ConfigurableServiceProvider<CoordinatesServiceProviderConfig> {
 	 * provider'
 	 */
 	public String getName() {
-		return "My example service";
+		return "Coordinate tools";
 	}
-	
+
 	@Override
 	public String toString() {
 		return getName();
 	}
-	
+
 	public String getId() {
 		return providerId.toASCIIString();
-	}
-
-	@Override
-	protected List<? extends Object> getIdentifyingData() {
-		// TODO Auto-generated method stub
-		//return null;
-		return Arrays.asList(getConfiguration().getUri());
 	}
 
 }
