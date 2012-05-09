24fd2d05280d450d788b37dc7f24bf6ac3dea5a5
Stian Soiland-Reyes <stian@soiland-reyes.com>
2012-05-09 16:16:01 +0100

    A kind of renderer

---
+++ votable-renderer/src/main/java/org/purl/wf4ever/astrotaverna/view/votable/VOTableRenderer.java
@@ -0,0 +1,44 @@
+package org.purl.wf4ever.astrotaverna.view.votable;
+
+
+import javax.swing.JComponent;
+
+import net.sf.taverna.t2.reference.ReferenceService;
+import net.sf.taverna.t2.reference.T2Reference;
+import net.sf.taverna.t2.renderers.Renderer;
+import net.sf.taverna.t2.renderers.RendererException;
+
+public class VOTableRenderer implements Renderer {
+
+	@Override
+	public boolean canHandle(String mimeType) {
+		return "application/x-votable+xml".equals(mimeType);
+	}
+
+	@Override
+	public boolean canHandle(ReferenceService referenceService,
+			T2Reference reference, String mimeType) throws RendererException {
+		if (reference.containsErrors() || reference.getDepth() > 0) {
+			return false;
+		}
+		if (canHandle(mimeType)) { 
+			return true;
+		} 
+		String asString = (String) referenceService.renderIdentifier(reference, String.class, null);
+		return asString.contains("http://www.ivoa.net/xml/VOTable");		
+	}
+
+	@Override
+	public JComponent getComponent(ReferenceService referenceService,
+			T2Reference reference) throws RendererException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getType() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
