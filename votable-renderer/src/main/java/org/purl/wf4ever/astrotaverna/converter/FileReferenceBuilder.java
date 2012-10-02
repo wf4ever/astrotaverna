e500f41890ed0fb07d3d361b6a439cee6efd217b
Stian Soiland-Reyes <stian@soiland-reyes.com>
2012-10-02 18:07:26 +0100

    File Reference Builder

---
+++ votable-renderer/src/main/java/org/purl/wf4ever/astrotaverna/converter/FileReferenceBuilder.java
@@ -0,0 +1,59 @@
+package org.purl.wf4ever.astrotaverna.converter;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+
+import net.sf.taverna.t2.reference.ExternalReferenceBuilderSPI;
+import net.sf.taverna.t2.reference.ReferenceContext;
+import net.sf.taverna.t2.reference.impl.external.file.FileReference;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.log4j.Logger;
+
+public class FileReferenceBuilder implements
+		ExternalReferenceBuilderSPI<FileReference> {
+
+	private static Logger logger = Logger
+			.getLogger(FileReferenceBuilder.class);
+
+	@Override
+	public FileReference createReference(InputStream byteStream,
+			ReferenceContext context) {
+		File tmpFile = null;
+		try {
+			tmpFile = File.createTempFile("taverna", ".tmp");
+			// TODO: Is it OK to delete on exit?
+			// tmpFile.deleteOnExit();
+			FileUtils.copyInputStreamToFile(byteStream, tmpFile);
+			logger.debug("Converted reference to file " + tmpFile + " ("
+					+ tmpFile.length() + " bytes)");
+			return new FileReference(tmpFile);
+		} catch (IOException e) {
+			String message = "Can't save data to " + tmpFile;
+			logger.warn(message);
+			throw new RuntimeException(message);
+		} finally {
+			try {
+				byteStream.close();
+			} catch (IOException e) {
+				logger.warn("Can't close rendering stream", e);
+			}
+		}
+	}
+
+	@Override
+	public Class<FileReference> getReferenceType() {
+		return FileReference.class;
+	}
+
+	@Override
+	public boolean isEnabled(ReferenceContext context) {
+		return true;
+	}
+
+	@Override
+	public float getConstructionCost() {
+		return 1.0f;
+	}
+}
