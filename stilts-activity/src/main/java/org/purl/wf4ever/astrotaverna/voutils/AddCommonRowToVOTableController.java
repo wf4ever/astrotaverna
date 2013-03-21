148efe505bc2eaaeb61be513bd404bb6b58b776a
julian <jgarrido@iaa.es>
2013-03-21 15:26:57 +0100

    first steps in AddCommonRowToVOTable

---
+++ stilts-activity/src/main/java/org/purl/wf4ever/astrotaverna/voutils/AddCommonRowToVOTableController.java
@@ -0,0 +1,105 @@
+package org.purl.wf4ever.astrotaverna.voutils;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
+
+import org.apache.log4j.Logger;
+
+import uk.ac.starlink.table.JoinStarTable;
+import uk.ac.starlink.table.RowListStarTable;
+import uk.ac.starlink.table.StarTable;
+import uk.ac.starlink.table.StarTableFactory;
+import uk.ac.starlink.table.StarTableOutput;
+import uk.ac.starlink.table.StarTableWriter;
+import uk.ac.starlink.table.TableFormatException;
+
+public class AddCommonRowToVOTableController {
+	private String rowVOTablePath;
+	private String mainVOTablePath;
+	private StarTable mainVOTable;
+	private StarTable rowVOTable;
+	
+	private StarTable result;
+	
+	private static Logger logger = Logger.getLogger(AddCommonRowToVOTableController.class);
+	
+	public AddCommonRowToVOTableController (String rowVOTablePath, String mainVOTablePathj) throws TableFormatException, IOException{
+		long colrowcount, rowrowcount, rownumber_main;
+		RowListStarTable commonRowTable;
+		JoinStarTable joinTable;
+		this.rowVOTablePath = rowVOTablePath;
+		this.mainVOTablePath = mainVOTablePath;
+		
+		
+		mainVOTable = loadVOTable(this.mainVOTablePath);
+		rowVOTable = loadVOTable(this.rowVOTablePath);
+		
+		if(mainVOTable !=null && rowVOTable !=null){
+			colrowcount = rowVOTable.getRowCount();
+			rowrowcount = rowVOTable.getRowCount();
+			rownumber_main = mainVOTable.getRowCount();
+			result = mainVOTable;
+			if(colrowcount > 0 && rowrowcount >0){
+				try {
+					//get the row
+					Object[] row = rowVOTable.getRow(0);
+					
+					//build the aux table
+					commonRowTable = new RowListStarTable(rowVOTable);
+					for(long i = 0; i<rownumber_main; i++){
+						commonRowTable.addRow(row);
+					}
+					
+					//join both tables
+					if(commonRowTable.getRowCount() == mainVOTable.getRowCount()){
+						StarTable [] tables = new StarTable[2];
+						tables[0] = mainVOTable;
+						tables[1] = commonRowTable;
+						
+						joinTable = new JoinStarTable(tables);
+						if(joinTable !=null)
+							result = joinTable;
+						
+					}
+					
+				} catch (IOException e) {
+					logger.warn("The table didn't have elements.\n " + e.getMessage(), e);
+				}
+				
+			}
+		}
+		
+	}
+	
+	public StarTable getJoinTable(){
+		return result;
+	}
+	
+	public void writeJoinTable(File file) throws TableFormatException, IOException{
+		StarTableOutput output = new StarTableOutput();
+		output.writeStarTable(result, file.toString(), "votable" );
+	}
+	
+	public String writeJoinTable() throws IOException{
+		ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+		writeTableAsVOTable(result, outputStream);
+		String string_result = outputStream.toString("UTF-8");
+		return string_result;
+	}
+		
+	private void writeTableAsVOTable( StarTable table, OutputStream out ) throws IOException {
+        StarTableOutput sto = new StarTableOutput();
+        StarTableWriter outputHandler = sto.getHandler( "votable" );
+        sto.writeStarTable( table, out, outputHandler );
+    }
+	
+	private StarTable loadVOTable(String path) throws TableFormatException, IOException{
+		return new StarTableFactory().makeStarTable(path, "votable" );
+	}
+	
+	public StarTable loadVOTable( File source ) throws IOException {
+	    return new StarTableFactory().makeStarTable( source.toString(), "votable" );
+	}
+}
---
+++ stilts-activity/src/test/java/org/purl/wf4ever/astrotaverna/voutils/AddCommonRowToVOTableControlllerTest.java
@@ -0,0 +1,145 @@
+package org.purl.wf4ever.astrotaverna.voutils;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Locale;
+import java.util.Map;
+import java.util.Set;
+
+import net.sf.taverna.t2.activities.testutils.ActivityInvoker;
+import net.sf.taverna.t2.workflowmodel.OutputPort;
+import net.sf.taverna.t2.workflowmodel.processor.activity.ActivityConfigurationException;
+import net.sf.taverna.t2.workflowmodel.processor.activity.ActivityInputPort;
+
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.purl.wf4ever.astrotaverna.utils.MyUtils;
+
+import uk.ac.starlink.table.StarTable;
+import uk.ac.starlink.table.StarTableFactory;
+/**
+ * 
+ * @author julian Garrido 
+ * Some tests may fail because the resulting votable name comes from a random number 
+ */
+public class AddCommonRowToVOTableControlllerTest {
+
+	private AddCommonRowToVOTableController controller;
+	
+		
+	
+
+	
+	@Test
+	public void joinTablesAndWriteInFile() throws Exception {
+		File file = File.createTempFile("astro", null);
+		
+		File tmpInFile = MyUtils.writeStringAsTmpFile(shortTable);
+		//tmpInFile.deleteOnExit();
+		String shortPath = tmpInFile.getAbsolutePath();
+		
+		File tmpInFile2 = MyUtils.writeStringAsTmpFile(mainTable);
+		//tmpInFile2.deleteOnExit();
+		String mainPath = tmpInFile.toString();//.getAbsolutePath();
+System.out.println("Path short: "+tmpInFile.getAbsolutePath());
+System.out.println("Path main: "+tmpInFile2.getAbsolutePath());
+		controller = new AddCommonRowToVOTableController(shortPath, mainPath);
+		file.deleteOnExit();
+		controller.writeJoinTable(file);
+		
+		StarTable starTable = loadVOTable(file.getAbsolutePath());
+		
+		// Should not change on reconfigure
+		assertEquals("Unexpected number of rows", 2, starTable.getRowCount());
+		assertEquals("Unexpected number of columns", 7, starTable.getColumnCount());
+	}
+
+	
+	public StarTable loadVOTable( String  path ) throws IOException {
+	    return new StarTableFactory().makeStarTable(path, "votable" );
+	}
+	
+	private static final String shortTable = 
+			"<?xml version='1.0'?>"
+					+ "<VOTABLE version=\"1.1\""
+					+ " xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\""
+					+ " xsi:schemaLocation=\"http://www.ivoa.net/xml/VOTable/v1.1 http://www.ivoa.net/xml/VOTable/v1.1\""
+					+ " xmlns=\"http://www.ivoa.net/xml/VOTable/v1.1\">"
+					+ "<RESOURCE>"
+					+ "<TABLE name=\"astro1467408768841096458.tmp\" nrows=\"2\">" 
+					+ "<PARAM datatype=\"float\" name=\"inputRA\" unit=\"degrees\" value=\"195.16333\"/>"
+					+ "<PARAM datatype=\"float\" name=\"inputDEC\" unit=\"degrees\" value=\"2.5007777\"/>"
+					+ "<PARAM datatype=\"float\" name=\"inputSR\" unit=\"degrees\" value=\"0.001\"/>"
+					+ "<FIELD ID=\"U\" datatype=\"float\" name=\"U\" ucd=\"PHOT_SDSS_U FIT_PARAM\"/>"
+					+ "<FIELD ID=\"G\" datatype=\"float\" name=\"G\" ucd=\"PHOT_SDSS_G FIT_PARAM\"/>"
+					+ "<DATA>"
+					+ "<TABLEDATA>"
+					+ "  <TR>"
+					+ "    <TD>10.52193</TD>"
+					+ "    <TD>10.47281</TD>"
+					+ "  </TR>"
+					+ "  <TR>"
+					+ "    <TD>13.7583</TD>"
+					+ "    <TD>12.79937</TD>"
+					+ "  </TR>"
+					+ "</TABLEDATA>"
+					+ "</DATA>"
+					+ "</TABLE>"
+					+ "</RESOURCE>"
+					+ "</VOTABLE>";
+	
+	private static final String mainTable = 
+			"<?xml version='1.0'?>"
+					+ "<VOTABLE version=\"1.1\""
+					+ " xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\""
+					+ " xsi:schemaLocation=\"http://www.ivoa.net/xml/VOTable/v1.1 http://www.ivoa.net/xml/VOTable/v1.1\""
+					+ " xmlns=\"http://www.ivoa.net/xml/VOTable/v1.1\">"
+					+ "<!--"
+					+ " !  VOTable written by STIL version 3.0-3 (uk.ac.starlink.votable.VOTableWriter)"
+					+ " !  at 2012-05-18T09:05:51"
+					+ " !-->"
+					+ "<RESOURCE>"
+					+ "<TABLE name=\"astro1467408768841096458.tmp\" nrows=\"2\">" 
+					+ "<PARAM datatype=\"float\" name=\"inputRA\" unit=\"degrees\" value=\"195.16333\"/>"
+					+ "<PARAM datatype=\"float\" name=\"inputDEC\" unit=\"degrees\" value=\"2.5007777\"/>"
+					+ "<PARAM datatype=\"float\" name=\"inputSR\" unit=\"degrees\" value=\"0.001\"/>"
+					+ "<PARAM arraysize=\"1\" datatype=\"char\" name=\"rowcount, table 0\" value=\"2\"/>"
+					+ "<FIELD ID=\"U\" datatype=\"float\" name=\"U\" ucd=\"PHOT_SDSS_U FIT_PARAM\"/>"
+					+ "<FIELD ID=\"G\" datatype=\"float\" name=\"G\" ucd=\"PHOT_SDSS_G FIT_PARAM\"/>"
+					+ "<FIELD ID=\"R\" datatype=\"float\" name=\"R\" ucd=\"PHOT_SDSS_R FIT_PARAM\"/>"
+					+ "<FIELD ID=\"I\" datatype=\"float\" name=\"I\" ucd=\"PHOT_SDSS_I FIT_PARAM\"/>"
+					+ "<FIELD ID=\"Z\" datatype=\"float\" name=\"Z\" ucd=\"PHOT_SDSS_Z FIT_PARAM\"/>"
+					+ "<DATA>"
+					+ "<TABLEDATA>"
+					+ "  <TR>"
+					+ "    <TD>17.52193</TD>"
+					+ "    <TD>17.47281</TD>"
+					+ "    <TD>17.50826</TD>"
+					+ "    <TD>17.99788</TD>"
+					+ "    <TD>17.8128</TD>"
+					+ "  </TR>"
+					+ "  <TR>"
+					+ "    <TD>13.7583</TD>"
+					+ "    <TD>12.79937</TD>"
+					+ "    <TD>12.27589</TD>"
+					+ "    <TD>11.99715</TD>"
+					+ "    <TD>11.71331</TD>"
+					+ "  </TR>"
+					+ "</TABLEDATA>"
+					+ "</DATA>"
+					+ "</TABLE>"
+					+ "</RESOURCE>"
+					+ "</VOTABLE>";
+	
+}
